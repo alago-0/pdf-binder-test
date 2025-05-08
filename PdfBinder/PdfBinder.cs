48d9d1d0910efba3fb963ebeba725ae17d99df50
Alexandr Lagornii <alexandrlagornii@gmail.com>
2025-05-08 19:03:15 +0300

    First Commit

---
+++ PdfBinder/PdfBinder.cs
@@ -0,0 +1,44 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PdfUtils
+{
+  public static class PdfBinder
+  {
+    public static byte[] BindPdfBytes(List<byte[]> pdfsBytes)
+    {
+      // Create new pdf and do actions on it
+      using (var outPdf = new PdfDocument())
+      {
+        // Iterate over each input pdf
+        foreach (var pdfBytes in pdfsBytes)
+        {
+          // Read pdf bytes as file
+          using (var pdfStream = new MemoryStream(pdfBytes))
+          {
+            using (var inputPdf = PdfReader.Open(pdfStream, PdfDocumentOpenMode.Import))
+            {
+              CopyPdfPages(inputPdf, outPdf);
+            }
+          }
+        }
+        
+        // Save as file (memory stream) and return bytes
+        using (var outPdfStream = new MemoryStream())
+        {
+          outPdf.Save(outPdfStream, false);
+          return outPdfStream.ToArray();
+        }
+      }
+    }
+
+    private static void CopyPdfPages(PdfDocument from,
+                                     PdfDocument to)
+    {
+      foreach (var page in from.Pages)
+      {
+        to.AddPage(page);
+      }
+    }
+  }
+}
---
+++ TestPdf/Program.cs
@@ -0,0 +1,22 @@
+﻿using PdfUtils;
+
+var basePath = @"C:\Users\UserPC\Desktop\Job\Projects\StareCivila\TestPdf\";
+var files = new List<string>
+{ 
+  @"Files\amd.pdf",
+  @"Files\dell.pdf",
+  @"Files\nvidia.pdf"
+};
+List<byte[]> pdfsBytes = new();
+foreach (var file in files)
+{
+  var path = basePath + file;
+  var pdfBytes = File.ReadAllBytes(path);
+  pdfsBytes.Add(pdfBytes);
+}
+
+var bindedPdf = PdfBinder.BindPdfBytes(pdfsBytes);
+using (FileStream fs = File.Create(basePath + "test.pdf"))
+{
+  fs.Write(bindedPdf, 0, bindedPdf.Length);
+}
